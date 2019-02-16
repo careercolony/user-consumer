91719f8ffcc2304b071aac1542e75985a31294f8
rushabh <rushabh_11490@yahoo.co.in>
2019-02-17 03:29:39 +0530

    user consumer changes

--- src/main/scala/MailGun.scala
+++ src/main/scala/MailGun.scala
@@ -7,7 +7,7 @@ import scala.concurrent.duration._
 
 object MailGun {
 
-  
+
   def sendMessage(message: RegisterDtoResponse) = {
     println("parsed message from topic:" + message)
     val mailgun = new Mailgun(domainName, apiKey)
@@ -16,7 +16,7 @@ object MailGun {
       to = EmailAddress(message.email, message.firstname),
       //to = EmailAddress("rushabh_11490@yahoo.co.in", message.firstname),
       subject,
-      html = "<html><body><div><br><br> Dear " + message.firstname + ", <br><br><b> Congratulations! Your account has been created successfully!!</b><br><br> Please login to the Carrier Colony website using the Login Id: " + message.email + "<br><br><br> Thank you for registering with Carrier Colony.<br><br><b>Best regards,</b><br> Carrier Colony Team<br><br>Note: This is a system generated e-mail, please do not reply to it.<br><br><br></body></html>"
+      html = "<html><body><div><br><br> Dear " + message.firstname + ", <br><b><h2> <font color=#32CD32>Congratulations! Your account has been created successfully!!</font></h2></b><br> Please login to the Carrier Colony website using the Login Id: " + message.email + "<br><br><br> Thank you for registering with Carrier Colony.<br><br><b>Best regards,</b><br> Carrier Colony Team<br><br>Note: This is a system generated e-mail, please do not reply to it.<br><br><br></body></html>"
     ))
 
     val result = Await.result(response, Duration.Inf)
